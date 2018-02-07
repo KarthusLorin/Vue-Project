5a5016bb16e9f1b5fb976a2662df4dd9b599d712
胡彬 <karthuslorin@gmail.com>
2018-02-08 00:44:37 +0800

    add mock data & vue-resource

--- eleme/sell/src/main.js
+++ eleme/sell/src/main.js
@@ -3,15 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
 
 import './common/stylus/index.styl'
 
+Vue.use(VueResource)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
