a60e45e6f5f7c2288abc1faaf9250ab19d03eb00
胡彬 <karthuslorin@gmail.com>
2018-03-09 00:15:47 +0800

    add

--- eleme/sell/src/components/header/header.vue
+++ eleme/sell/src/components/header/header.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="content-wrapper">
     <div class="bulletin-wrapper">
-      <div class="avatar"></div>
+      <div class="avatar">
+        <img width="64" height="64" :src="seller.avatar">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{ seller.name }}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div v-if="seller.supports" class="support">
+          <span class="icon"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,6 +31,14 @@
   }
 </script>
 
-<style scoped>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  .header
+    color: #fff
+    background: #000
+    .content-wrapper
+      padding: 24px 12px 18px 24px
+      .avatar
+        display: inline-block
+      .content
+        display: inline-block
 </style>
