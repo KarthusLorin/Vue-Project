0bf1a95ed6e824325c4aebfadd11bcd3e5bbce06
胡彬 <karthuslorin@gmail.com>
2018-03-11 23:47:52 +0800

    add

--- eleme/sell/src/components/header/header.vue
+++ eleme/sell/src/components/header/header.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="content-wrapper">
-    <div class="bulletin-wrapper">
-      <div class="avatar">
-        <img width="64" height="64" :src="seller.avatar">
-      </div>
-      <div class="content">
-        <div class="title">
-          <span class="brand"></span>
-          <span class="name">{{ seller.name }}</span>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="bulletin-wrapper">
+        <div class="avatar">
+          <img width="64" height="64" :src="seller.avatar">
         </div>
-        <div class="description">
-          {{seller.description}}/{{seller.deliveryTime}}分钟送达
-        </div>
-        <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
-          <span class="text">{{seller.supports[0].description}}</span>
+        <div class="content">
+          <div class="title">
+            <span class="brand"></span>
+            <span class="name">{{ seller.name }}</span>
+          </div>
+          <div class="description">
+            {{seller.description}}/{{seller.deliveryTime}}分钟送达
+          </div>
+          <div v-if="seller.supports" class="support">
+            <span class="icon"></span>
+            <span class="text">{{seller.supports[0].description}}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -32,13 +34,50 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin"
+
   .header
-    color: #fff
-    background: #000
+    color #fff
+    background #000
     .content-wrapper
-      padding: 24px 12px 18px 24px
+      padding 24px 12px 18px 24px
+      font-size 0
       .avatar
-        display: inline-block
+        display inline-block
+        vertical-align top
+        margin-left 16px
+        img
+          border-radius 2px
       .content
-        display: inline-block
+        display inline-block
+        margin-left 14px
+        font-size 14px
+        .title
+          margin 2px 0 8px 0
+          .brand
+            display inline-block
+            vertical-align middle
+            width: 30px
+            height 18px
+            bg-image('brand')
+            background-size 30px 18px
+            background-repeat no-repeat
+          .name
+            margin-left 6px
+            font-size 16px
+            line-height 18px
+            font-weight bold
+            vertical-align middle
+        .description
+          margin-bottom 10px
+          line-height 12px
+          font-size 12px
+        .support
+          .icon
+            display inline-block
+            width 12px
+            height 12px
+            margin-right 4px
+            background-size 12px 12px
+            background-repeat no-repeat
 </style>
