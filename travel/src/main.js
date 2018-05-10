6e15ffa0837b69147af8421a9e6803eea09ca14d
KarthusLorin <karthuslorin@gmail.com>
2018-05-10 23:09:34 +0800

    project init

--- travel/src/main.js
+++ travel/src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 因为在移动端中，点击事件有300ms的延迟，影响用户体验，采用fastclick可以去除该延迟
+import fastClick from 'fastclick'
+// 初始化css
+import './assets/styles/reset.css'
+// 解决1px的问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 在body上绑定fastClick
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
