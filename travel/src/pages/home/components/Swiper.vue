88d473c2283755855f49b822be48ee3066ac9a28
KarthusLorin <karthuslorin@gmail.com>
2018-05-14 23:40:32 +0800

    add

--- travel/src/pages/home/components/Swiper.vue
+++ travel/src/pages/home/components/Swiper.vue
@@ -40,10 +40,10 @@ export default {
     /*
       padding的百分比是相对于父元素宽度的
       父元素宽度和该元素宽度一样，所以：
-      这种写法就的高度相当于宽度的31.25%
-      如果，直接写height为31.25%，则相当于父元素高度的31.25%，不符
+      这种写法就的高度相当于宽度的26.7%
+      如果，直接写height为26.7%，则相当于父元素高度的26.7%，不符
      */
-    padding-bottom 31.25%
+    padding-bottom 26.7%
     background: #eee
     .swiper-img
       width: 100%
