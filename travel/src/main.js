2e4cba41d195a6a45a7cd027b3ab58f4ff35ef51
KarthusLorin <karthuslorin@gmail.com>
2018-05-14 23:22:01 +0800

    add Swiper

--- travel/src/main.js
+++ travel/src/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import router from './router'
 // 因为在移动端中，点击事件有300ms的延迟，影响用户体验，采用fastclick可以去除该延迟
 import fastClick from 'fastclick'
+// 轮播图插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 // 初始化css
 // styles是assets下styles文件夹的别名
 import 'styles/reset.css'
@@ -12,10 +14,14 @@ import 'styles/reset.css'
 import 'styles/border.css'
 // iconfont
 import 'styles/iconfont.css'
+// 轮播图插件样式
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 // 在body上绑定fastClick
 fastClick.attach(document.body)
+// 使用轮播图
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/15/9250dbc86a456302.jpg_750x200_b423f532.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/c4/1cdd28811593b802.jpg_750x200_5fbb7c91.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    width: 100%
+    height: 0
+    overflow: hidden
+    /*
+      padding的百分比是相对于父元素宽度的
+      父元素宽度和该元素宽度一样，所以：
+      这种写法就的高度相当于宽度的31.25%
+      如果，直接写height为31.25%，则相当于父元素高度的31.25%，不符
+     */
+    padding-bottom 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
