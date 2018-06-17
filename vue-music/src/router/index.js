4e644d1ef5e2227c6876a28c2817d2fa4c2b6568
KarthusLorin <karthuslorin@gmail.com>
2018-06-17 21:52:24 +0800

    init vue-music

--- vue-music/src/router/index.js
+++ vue-music/src/router/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import 'common/stylus/index.styl'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: []
 })
